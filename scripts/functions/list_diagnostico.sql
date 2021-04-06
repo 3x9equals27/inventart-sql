c36d0a68d65cad9a65ed4081176e9947b1013498
heldersousa <3x9equals27@gmail.com>
2021-04-06 23:03:18 +0200

    return file guid when listing diagnostics

--- scripts/functions/list_diagnostico.sql
+++ scripts/functions/list_diagnostico.sql
@@ -2,14 +2,17 @@ DROP FUNCTION IF EXISTS list_diagnostico;
 CREATE OR REPLACE FUNCTION list_diagnostico()
   RETURNS TABLE (guid uuid   -- also visible as OUT parameter inside function
                , description VARCHAR
-               , has_file boolean) AS
+               , has_file boolean
+			   , file_guid uuid) AS
 $func$
 BEGIN
     RETURN QUERY
     SELECT d.guid
          , d.description
-         , d.id_file IS NOT NULL AS has_file 
+         , d.id_file IS NOT NULL AS has_file
+         , f.guid AS file_guid
       FROM diagnostico d
+      LEFT JOIN file f ON f.id = d.id_file
 	 ORDER BY d.id ASC
 	;
 END
